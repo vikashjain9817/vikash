735f0363592814e7b8a1bf34e36eb78f2a80ba40
vikashjain9817 <vikashjain@github.com>
2018-01-29 19:29:01 +0530

    alphabet
    it will check whether the given charcter is alphabet or not.

---
+++ alphabate.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void main()
+{
+    char c;
+    scanf("%c",&c);
+
+    if( (c>='a' && c<='z') || (c>='A' && c<='Z'))
+        printf(""Alphabet);
+    else
+        printf("No");
+
+}

83c5c8ca5fb820861a927581fbbbd12c6f241d17
vikashjain9817 <vikashjain@github.com>
2018-01-29 18:46:36 +0530

    Revert "Merge branch 'master' of https://github.com/vikashjain9817/vikash"
    This reverts commit fefcd72c66e7cb901de1e816c5ed9305578a33be, reversing
    changes made to 7d6d608f787720110c42146989d7e6c9d49ee762.

--- even.c
+++ even.c
@@ -1,5 +1,4 @@
 <<<<<<< HEAD
-<<<<<<< HEAD
 #include<stdio.h>
 void main()
 {
@@ -11,19 +10,6 @@ void main()
 	else{
 		printf("odd");
 	}
-=======
-#include<stdio.h>
-void main()
-{
-	double n;
-	scanf("%ld",&n);
-	if(n%2==0){
-		printf("even");
-	}
-	else{
-		printf("odd");
-	}
->>>>>>> d1a9963ceb601ef9a432451ee22c2dc1f1402768
 =======
 #include<stdio.h>
 void main()

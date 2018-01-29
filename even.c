7d6d608f787720110c42146989d7e6c9d49ee762
vikashjain9817 <vikashjain@github.com>
2018-01-29 18:16:21 +0530

    even number
    to check even or odd numbers

---
+++ even.c
@@ -0,0 +1,26 @@
+<<<<<<< HEAD
+#include<stdio.h>
+void main()
+{
+	double n;
+	scanf("%ld",&n);
+	if(n%2==0){
+		printf("even");
+	}
+	else{
+		printf("odd");
+	}
+=======
+#include<stdio.h>
+void main()
+{
+	double n;
+	scanf("%ld",&n);
+	if(n%2==0){
+		printf("even");
+	}
+	else{
+		printf("odd");
+	}
+>>>>>>> d1a9963ceb601ef9a432451ee22c2dc1f1402768
+}

beb025c78983b23c44d80a4909e065e24fd33013
vikashjain9817 <vikashjain@github.com>
2018-01-30 08:02:56 +0530

    palandrome
    it will find whether a number is palandrome or not?

---
+++ palandrome.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+	int n,r,n_reverse=0,orginal;
+	scanf("%d",&n);
+	orginal=n;
+	while(n!=0){
+		r=n%10;
+		n_reverse=n_reverse*10+r;
+		n=n/10;
+	}
+	if(n_reverse==orginal){
+		printf("yes");
+	}
+	else{
+		printf("no");
+	}
+}

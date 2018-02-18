56f19150c3393dbf0a0c490abe3b6f4be1a8035e
vikashjain9817 <vikashjain@github.com>
2018-02-18 21:57:04 +0530

    swap even and odd index charcter
    in this program we will swap the even nuber of index charcter with the odd numbers of charcter.
         the logic is that first of all a loop will execute 0 to length of the string. and it will increase by 2 at each itaration.
       and inside loop there is a logic of swap logic which swap the even number of chrcter with the odd numbers of charcter.

---
+++ swap_even_odd_charcter.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+void main(){
+    int i;
+    char a[100],b,temp;
+    gets(a);
+    int l=strlen(a);
+    for(i=0;i<l;i=i+2)
+	{
+        temp=a[i+1];
+        a[i+1]=a[i];
+        a[i]=temp;
+
+    }
+    puts(a);
+}
